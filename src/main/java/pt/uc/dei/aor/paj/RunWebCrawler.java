6c9c15ccbe3e55fbfac9caceea1c6a12d2a54919
renatadiasilva <renatadiasilva@gmail.com>
2015-06-13 19:00:01 +0100

    faz deploy(?) e tem crawler

--- src/main/java/pt/uc/dei/aor/paj/RunWebCrawler.java
+++ src/main/java/pt/uc/dei/aor/paj/RunWebCrawler.java
@@ -1,32 +1,34 @@
 package pt.uc.dei.aor.paj;
 
+import java.io.File;
 import java.util.Date;
+
+import pt.uc.dei.aor.paj.crawler.Crawler;
+import pt.uc.dei.aor.paj.xml.JAXBHandler;
+//import pt.uc.dei.aor.paj.data.Noticias;
 
 public class RunWebCrawler {
 	
 	public static void main(String[] args) throws Exception {
 		
 		//chamar o parser -> dá origem ao XML
-		
+//		Crawler c = new Crawler();
+//		Noticias noticias = c.doCrawler();
+
+		//passa para XML
+//		try {
+//			JAXBHandler.marshal(noticias, new File ("output.xml"));
+//			System.out.println("Marshall ok");//RETIRAR
+//		} catch (IOException | JAXBException e) {
+//			System.out.println(e.getMessage());
+//		}	
+
 		//enviar XML (como String) transformações?
+		// cena quirino
 		//passar xml ou a string como argumento
 		Publisher p = new Publisher();
 		p.publish();  // p.publish(stringXML);
-		
-		int[] count = {12, 20, 40, 7, 10, 33, 14};
-		//enviar o texto da estatística como argumento
-		String stats = "Statistics "+new Date()+"\n";
-		stats += "Americas -"+count[0];
-		stats += "Africa -"+count[1];
-		stats += "Asia -"+count[2];
-		stats += "China -"+count[3];
-		stats += "Europe -"+count[4];
-		stats += "Middle East -"+count[5];
-		stats += "U.S. -"+count[6];
-		
-//		Publisher p = new Publisher();  //é necessário?
-//		p.publish(stats);
-		
-		//OK
+
+		// para stats é igual
 	}
 }
