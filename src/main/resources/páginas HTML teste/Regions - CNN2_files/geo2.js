b338845cbe0624d3e1b413c01a9eeaf6340226eb
renatadiasilva <renatadiasilva@gmail.com>
2015-06-15 13:53:46 +0100

    guardar ficheiros

---
+++ src/main/resources/páginas HTML teste/Regions - CNN2_files/geo2.js
@@ -0,0 +1,16 @@
+(function(){
+  window['optimizely'] = window['optimizely'] || [];
+  window['optimizely'].push(['activateGeoDelayedExperiments', {
+    'location':{
+      'city': "COIMBRA",
+      'continent': "EU",
+      'country': "PT",
+      'region': ""
+    },
+    'ip':"194.210.173.181"
+  }]);
+})
+//
+()
+
+;
